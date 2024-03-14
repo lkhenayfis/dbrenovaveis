45f2fa931b98cbc22826418e42dee1165d752773
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2024-03-14 15:56:31 -0300

    Reformula testes de switch_reader_func

--- tests/testthat/test-classe-tabelas.r
+++ tests/testthat/test-classe-tabelas.r
@@ -11,59 +11,63 @@ test_that("Criacao de objetos 'campo'", {
     expect_error(campo_erro <- new_campo("teste_erro", "tipo_qualquer"))
 })
 
-compfun1 <- function(x, ...) aws.s3::s3read_using(FUN = data.table::fread, object = x, ...)
-compfun2 <- function(x, ...) aws.s3::s3read_using(FUN = arrow::read_parquet, object = x, ...)
-compfun3 <- function(x, ...) aws.s3::s3read_using(FUN = jsonlite::read_json, object = x, ...)
-
 test_that("Selecao de reader_fun", {
 
     # CSV ----------------------------------------------------------------
 
     ff <- switch_reader_func(".csv", FALSE)
-    expect_equal(ff, data.table::fread)
+    expect_equal(ff, inner_csv)
 
     ff <- switch_reader_func("csv", FALSE)
-    expect_equal(ff, data.table::fread)
+    expect_equal(ff, inner_csv)
 
     ff <- switch_reader_func(".csv", TRUE)
-    expect_equal(formals(ff), formals(compfun1))
-    expect_equal(body(ff), body(compfun1))
+    expect_equal(ff, outer_s3(inner_csv))
 
     ff <- switch_reader_func("csv", TRUE)
-    expect_equal(formals(ff), formals(compfun1))
-    expect_equal(body(ff), body(compfun1))
+    expect_equal(ff, outer_s3(inner_csv))
 
     # PARQUET ------------------------------------------------------------
 
     ff <- switch_reader_func(".parquet", FALSE)
-    expect_equal(ff, arrow::read_parquet)
+    expect_equal(ff, inner_parquet)
 
     ff <- switch_reader_func("parquet", FALSE)
-    expect_equal(ff, arrow::read_parquet)
+    expect_equal(ff, inner_parquet)
 
     ff <- switch_reader_func(".parquet", TRUE)
-    expect_equal(formals(ff), formals(compfun2))
-    expect_equal(body(ff), body(compfun2))
+    expect_equal(ff, outer_s3(inner_parquet))
 
     ff <- switch_reader_func("parquet", TRUE)
-    expect_equal(formals(ff), formals(compfun2))
-    expect_equal(body(ff), body(compfun2))
+    expect_equal(ff, outer_s3(inner_parquet))
+
+    # PARQUET.GZIP -------------------------------------------------------
+
+    ff <- switch_reader_func(".parquet.gzip", FALSE)
+    expect_equal(ff, inner_parquet_gzip)
+
+    ff <- switch_reader_func("parquet.gzip", FALSE)
+    expect_equal(ff, inner_parquet_gzip)
+
+    ff <- switch_reader_func(".parquet.gzip", TRUE)
+    expect_equal(ff, outer_s3(inner_parquet_gzip))
+
+    ff <- switch_reader_func("parquet.gzip", TRUE)
+    expect_equal(ff, outer_s3(inner_parquet_gzip))
 
     # JSON ---------------------------------------------------------------
 
     ff <- switch_reader_func(".json", FALSE)
-    expect_equal(ff, jsonlite::read_json)
+    expect_equal(ff, inner_json)
 
     ff <- switch_reader_func("json", FALSE)
-    expect_equal(ff, jsonlite::read_json)
+    expect_equal(ff, inner_json)
 
     ff <- switch_reader_func(".json", TRUE)
-    expect_equal(formals(ff), formals(compfun3))
-    expect_equal(body(ff), body(compfun3))
+    expect_equal(ff, outer_s3(inner_json))
 
     ff <- switch_reader_func("json", TRUE)
-    expect_equal(formals(ff), formals(compfun3))
-    expect_equal(body(ff), body(compfun3))
+    expect_equal(ff, outer_s3(inner_json))
 })
 
 test_that("Criacao de tabelas -- manual", {
@@ -85,7 +89,7 @@ test_that("Criacao de tabelas -- manual", {
     expect_equal(attr(tabela1, "uri"), "/qualquer/caminho/local/")
     expect_equal(attr(tabela1, "tipo_arquivo"), ".csv")
     expect_true(is.null(attr(tabela1, "descricao")))
-    expect_equal(attr(tabela1, "reader_func"), data.table::fread)
+    expect_equal(attr(tabela1, "reader_func"), inner_csv)
     expect_equal(attr(tabela1, "master"), data.table::data.table(tabela = character(0)))
 })
 
@@ -101,7 +105,7 @@ test_that("Criacao de tabelas -- dado teste s/ particao", {
     expect_equal(class(tabela1), c("tabela_local", "tabela"))
     expect_equal(attr(tabela1, "uri"), dir)
     expect_equal(attr(tabela1, "tipo_arquivo"), ".parquet.gzip")
-    expect_equal(attr(tabela1, "reader_func"), arrow::read_parquet)
+    expect_equal(attr(tabela1, "reader_func"), inner_parquet_gzip)
 
     master <- attr(tabela1, "master")
     expect_equal(nrow(master), 1)
@@ -120,7 +124,7 @@ test_that("Criacao de tabelas -- dado teste c/ particao", {
     expect_equal(class(tabela1), c("tabela_local", "tabela"))
     expect_equal(attr(tabela1, "uri"), dir)
     expect_equal(attr(tabela1, "tipo_arquivo"), ".parquet.gzip")
-    expect_equal(attr(tabela1, "reader_func"), arrow::read_parquet)
+    expect_equal(attr(tabela1, "reader_func"), inner_parquet_gzip)
 
     master <- attr(tabela1, "master")
     expect_equal(nrow(master), 2)
