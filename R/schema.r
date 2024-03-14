9327cce88fd0cee500900af0bebb1dc0256955ab
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2024-03-14 15:37:13 -0300

    Fecha composicao de schema

--- R/schema.r
+++ R/schema.r
@@ -1,11 +1,41 @@
 ################################### VALIDACAO DE JSONS DE SCHEMA ###################################
 
+compoe_schema <- function(arq_schema_banco) {
+
+    is_abs_path <- xfun::is_abs_path(arq_schema_banco)
+
+    rf <- switch_reader_func("json", grepl("^s3", arq_schema_banco))
+    schema_banco <- rf(arq_schema_banco)
+
+    tem_uri_root <- !is.null(schema_banco$uri)
+    if (!tem_uri_root && is_abs_path) schema_banco$uri <- sub("/schema.json", "", arq_schema_banco)
+
+    schema_banco <- valida_schema_banco(schema_banco)
+
+    nomes_tabs <- sapply(schema_banco$tables, "[[", "name")
+    schema_banco$tables <- lapply(schema_banco$tables, function(s_t) {
+        out <- rf(file.path(s_t$uri, "schema.json"))
+
+        # os schemas de tabelas individuais nao necessariamente vao ter as uris absolutas, ou mesmo
+        # certas (embora corrigir isso seja um efeito colateral de corrigir as uris relativas). Por
+        # este motivo e feita a substituicao de out$uri pelo caminho de onde out foi lido
+        out$uri <- s_t$uri
+        valida_schema_tabela(out)
+    })
+
+    return(schema_banco)
+}
+
+# VALIDADORES --------------------------------------------------------------------------------------
+
 valida_schema_banco <- function(schema) {
 
     if (is.null(schema$tables)) {
         stop("Schema do banco nao contem lista de tabelas")
     }
 
+    # mesmo que algo parecido aconteca na funcao acima, e possivel que ainda chegue aqui sem ter uma
+    # uri root. Por si so nao e um problema, mas vai ser se as tabelas nao tiverem uris absolutas
     tem_uri_root <- !is.null(schema$uri)
     uri_root_abs <- tem_uri_root && xfun::is_abs_path(schema_banco$uri)
     tab_uris <- sapply(schema$tables, "[[", "uri")
@@ -39,8 +69,8 @@ valida_schema_tabela <- function(schema) {
 
     # validacao de tipo de arquivo ---------------------------------------
 
-    tipo_permitido <- valida_tipo_arquivo(schema$fileType)
-    schema <- tipo_permitido # substitui para o caso de schema$fileType nao iniciar com '.'
+    tipo_permitido  <- valida_tipo_arquivo(schema$fileType)
+    schema$fileType <- tipo_permitido # substitui para o caso de schema$fileType nao iniciar com '.'
 
     # validacoes de colunas ----------------------------------------------
 
